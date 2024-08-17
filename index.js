73a947a9e281ac9d146f43877a9c4328112cbc1b
HANSAMAL/LK <atayafuataya@gmail.com>
2024-08-17 10:01:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ require("./plugins/" + plugin);
 console.log('Plugins installed successful ✅')
 console.log('Bot connected to whatsapp ✅')
 
-let up = `Wa-BOT connected successful ✅\n\nPREFIX: ${prefix}`;
+let up = `HANSAMAL-MD connected successful ✅\n\nPREFIX: ${prefix}`;
 
 conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://telegra.ph/file/44826e95f6e863548e408.jpg` }, caption: up })
 
