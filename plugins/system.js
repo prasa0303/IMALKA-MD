9dcf15bfd2073b549ed6559e115a4cf265b05205
HANSAMAL/LK <atayafuataya@gmail.com>
2024-08-19 15:47:48 +0530

    👩‍💻Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -14,7 +14,7 @@ async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender
 try{
   let status = `*Uptime:*  ${runtime(process.uptime())}
 *Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
-*HostName:* ${os.hostname()}
+*HostName:* HANSAMAL-MD
 *Owner:* 👨‍💻IMALKA HANSAMAL
 `
 return reply(`${status}`)
