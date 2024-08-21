2a8cd1a69fcd5e6d2f2b0286b0efae06603f6987
HANSAMAL/LK <atayafuataya@gmail.com>
2024-08-21 14:20:21 +0530

    👨‍💻index📍

--- index.js
+++ index.js
@@ -134,6 +134,12 @@ conn.sendFileUrl = async (jid, url, caption, quoted, options = {}) => {
               }
             }
 
+//===========================WORK_type================================================= 
+if (!isOwner && config.MODE === "private") return 
+if (!isOwner && isGroup && config.MODE === "inbox") return
+if (!isOwner && !isGroup && config.MODE === "groups") return
+//============================================================================
+
 
 const events = require('./command')
 const cmdName = isCmd ? body.slice(1).trim().split(" ")[0].toLowerCase() : false;
@@ -165,14 +171,7 @@ mek.type === "stickerMessage"
 ) {
 command.function(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply})
 }});
-//===========================WORK_type================================================= 
 
-if (!isOwner && config.MODE === "private") return 
-if (!isOwner && isGroup && config.MODE === "inbox") return
-if (!isOwner && !isGroup && config.MODE === "groups") return
-//============================================================================
-
-})
 }
 app.get("/", (req, res) => {
 res.send("hey, bot started✅");
