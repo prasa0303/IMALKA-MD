d3ae7846ce2a881bd286d8c02eae632db8856db0
HANSAMAL/LK <atayafuataya@gmail.com>
2024-08-25 12:06:37 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -5,7 +5,7 @@ const {runtime} = require('../lib/functions')
 
 cmd({
     pattern: "system",
-    alias:["status",botinfo]
+    alias:["status","botinfo"],
     desc: "Check up time , ram usage and more",
     category: "main",
     filename: __filename
