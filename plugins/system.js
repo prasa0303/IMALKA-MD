cad968fe29bba0ddef49e58a507cb9a8fffbaee0
HANSAMAL/LK <atayafuataya@gmail.com>
2024-08-27 15:32:57 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -12,10 +12,10 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-  let status = `*Uptime:* ${runtime(process.uptime())} 
-*Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
-*HostName:* ${os.hostname()}
-*Owner:*IMALKA HANSAMAL
+  let status = `┆*Uptime:* ${runtime(process.uptime())} 
+┆*Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+┆*HostName:* 😍HANSAMAL-MD
+┆*Owner:*📍IMALKA HANSAMAL👨‍💻
 `
 return reply(`${status}`)
 
