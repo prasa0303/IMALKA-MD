86837b90c380517a22ea71a26b795142723e5336
HANSAMAL/LK <atayafuataya@gmail.com>
2024-08-29 05:28:01 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -1,27 +1,28 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
 const os = require("os")
-const {runtime} = require('../lib/functions')
-
+const {runtime} = require(`../lib/functions`)
 cmd({
     pattern: "system",
-    alias:["status","botinfo"],
+    alias : ["status","botinfo"],
     desc: "Check up time , ram usage and more",
     category: "main",
+    react: "🧬",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-  let status = `*Uptime:* ${runtime(process.uptime())} 
-*Ram usage:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
-*HostName:* HANSAMAL-MD
-*Owner:* 📍IMALKA HANSAMAL👨‍💻
-`
+let status = `⏰ 𝗨𝗽 𝗧𝗶𝗺𝗲 : ${runtime(process.uptime())}
+📻 𝗥𝗮𝗺 : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+⚙️ 𝗛𝗼𝘀𝘁 : ${os.hostname()}
+👑 𝗢𝘄𝗻𝗲𝗿 : IMALKA-HANSAMAL‎`
 return reply(`${status}`)
-
 
 }catch(e){
 console.log(e)
 reply(`${e}`)
+
 }
-})
+} )
+
+  
