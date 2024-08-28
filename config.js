ae814e94d68aba1332b19c112e4071f3b0cb61cf
HANSAMAL/LK <atayafuataya@gmail.com>
2024-08-28 17:26:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,4 +7,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 MONGODB: process.env.MONGODB || "enter mongo url",
+PEXELS_API_KEY: process.env.PEXELS_API_KEY || "39WCzaHAX939xiH22NCddGGvzp7cgbu1VVjeYUaZXyHUaWlL1LFcVFxH",
+OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
 };
