0afc407b5032cf9ae7256cb9fb7ed4356457fe26
HANSAMAL/LK <atayafuataya@gmail.com>
2024-09-04 16:26:59 +0530

    Update main.js

--- plugins/main.js
+++ plugins/main.js
@@ -4,71 +4,64 @@ cmd({
     pattern: "menu",
     desc: "menu the bot",
     category: "menu",
-    react: "🗃️",
+    react: "📜",
     filename: __filename
 },
 
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-let dec = `*✦ SHADOW MD MENU LIST ✦*
+let menu = {
 
-> *╭──────────⚬➨*
+main: '',
+download: '',
+group: '',
+owner: '',
+convert: '',
+search: ''
 
-> *│👨🏻‍💻 COMMANDS LIST*
+};
 
-> *│   ───────*
+for (let i = 0; i < commands.length; i++) {
+if (commands[i].pattern && !commands[i].dontAddCommandList) {
+menu[commands[i].category] += `.${commands[i].pattern}\n`;
+ }
+}
 
-    ❮𝗛𝗢𝗪 𝘁𝗼 𝗗𝗢𝗪𝗡𝗟𝗢𝗗❯     
+let madeMenu = `👋🏻 *Hello ${pushname}*
 
-         *► .list*
+> *DOWNLOAD MENU* ⬇️
 
-         *▻Desc 📩* *-----==========-----*
+${menu.download}
 
-*❮► song DOWNLOAD*     
+> *MAIN MENU* 📃
 
-*❮►video DOWNLOD*               
+${menu.main}
 
-*-----===========-----* 
+> *GROUP MENU*
 
-*❮►FB DOWNLOAD*  
+${menu.group} 👨‍👨‍👦
 
-*❮►Tik Tok Download*      
+> *OWNER MENU* 👨‍💻
 
-*❮►twitter Download* 
+${main.owner}
 
-*-----===========-----*
+> *CONVERT MENU* ♻️
 
-*❮►mediafire Download*
+${menu.convert}
 
-*❮►googe drive Download* 
+> *SEARCH MENU* 🔍
 
- 
+${menu.search}
 
-𝗛𝗢𝗪 𝗧𝗢 𝗖𝗢𝗠𝗠𝗔𝗡𝗗 🗃️
+POWERED BY IMALKA-HANSAMAL 💚
 
-*❯-ʜᴏᴡ ᴛᴏ ʙᴏᴛ ꜱʏꜱᴛᴇᴍ*
 
-*❯-ʙᴏᴛ ᴘɪɴɢ ᴄʜᴀᴄᴋ*
 
-*❯-ᴍᴀɪɴ ᴏᴡɴᴇʀ ᴄʜᴇᴄᴋ*
-
-*❯-ʀᴇᴘᴏ ʟɪɴᴋ*
-
-   
-
- *❪Type..❫*
-
- *❮❮ .list*
-
-> ►.list all how to download command 👑👨🏻‍💻
-
-> *➣ OWNER : HANSAMAL-MD*
-*╰───────────⚬➨*
 `
-await conn.sendMessage(from,{image:{url: `https://telegra.ph/file/4c8ad037112cb4836b863.jpg`},caption:dec},{quoted:mek});
-await conn.sendMessage(from, { react: { text: '🗃️', key: mek.key }})
-await conn.sendMessage(from, {audio: { url: 'https://github.com/vipshadowmd/Voice/raw/main/SHADOW%20MD%20MENU.mp3' },mimetype: 'audio/mp4',ptt: true},{ quoted: mek });
+await conn.sendMessage(from,{image:{url:"https://telegra.ph/file/c718b67d351c1190e285b.jpg"},caption:medeMenu},{quoted.mek})
+
+
 }catch(e){
 console.log(e)
 reply(`${e}`)
