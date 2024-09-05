83059afc7a5f54d9b7e8e4438b9b8724444f0ba3
HANSAMAL/LK <atayafuataya@gmail.com>
2024-09-05 08:54:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,19 +22,16 @@ const { File } = require('megajs')
 
 const ownerNumber = ['94711262551']
 
-//===================SESSION============================
+//===================SESSION-AUTH============================
 if (!fs.existsSync(__dirname + '/auth_info_baileys/creds.json')) {
-  if (config.SESSION_ID) {
-    const sessdata = config.SESSION_ID.replace("VAJIRA-MD=", "")
-    const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
-    filer.download((err, data) => {
-      if (err) throw err
-      fs.writeFile(__dirname + '/auth_info_baileys/creds.json', data, () => {
-        console.log("Session download completed !!")
-      })
-    })
-  }
-}
+if(!config.SESSION_ID) return console.log('Please add your session to SESSION_ID env !!')
+const sessdata = config.SESSION_ID
+const filer = File.fromURL(`https://mega.nz/file/${sessdata}`)
+filer.download((err, data) => {
+if(err) throw err
+fs.writeFile(__dirname + '/auth_info_baileys/creds.json', data, () => {
+console.log("Session downloaded ✅")
+})})}
 
 const express = require("express");
 const app = express();
