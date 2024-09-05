99118101199fa75581761254809b0587d39cef7b
HANSAMAL/LK <atayafuataya@gmail.com>
2024-09-05 12:15:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "3BozDJxD#w2EN81MMVpCHnQsYLczxXPBd6f1D-3uhgqKUNdU0mQg",
-MONGODB: process.env.MONGODB || "enter mongo url",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 PEXELS_API_KEY: process.env.PEXELS_API_KEY || "39WCzaHAX939xiH22NCddGGvzp7cgbu1VVjeYUaZXyHUaWlL1LFcVFxH",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
 };
