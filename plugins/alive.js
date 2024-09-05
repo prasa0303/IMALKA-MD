ccdad63c6965c34d637720201b00327590ff42e7
HANSAMAL/LK <atayafuataya@gmail.com>
2024-09-05 12:13:51 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,24 +1,48 @@
-const config = require('../lib/database')
-const {cmd , commands} = require('../command')
+const config = require('../config')
+const { cmd, commands } = require('../command')
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
+const { Maker } = require('imagemaker.js')
+var imgmsg = "```Please give me a name !```"
+var imgmsg2 = "*Ex: .banner imalka+hansamal*"
+var imgmsg3 = "*Ex: .banner2 imalka+hansamal*"
+var imgmsg4 = "*Ex: .banner3 imalka+hansamal*"
+var imgmsg5 = "*Ex: .banner3 imalka+hansamal*"
+var imgmsg6 = "*Ex: .banner3 imalka+hansamal*"
+var desc = "It creates ephoto360 logos using 1 word.."
+var desc2 = "It creates channel banners.."
+var errt = "*I cant create this logo :(*"
 
 cmd({
-    pattern: "alive",
-    desc: "Check bot online or no.",
-    category: "main",
-    react: "👋🏻",
+    pattern: "logo",
+    react: '🎭',
+    alias: ["logo1","ephoto360","ephoto"],
+    desc: desc,
+    category: "logo",
+    use: '.ephoto360 ayodya vichakshana',
     filename: __filename
 },
-async(conn, mek, m, {from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
-    try {
-        // Send the audio message first
-        await conn.sendMessage(from, {
-            audio: { url: 'https://github.com/cobrs11/IMALKA-MD/raw/main/Media/Gm_A.mp3' },
-            mimetype: 'audio/mp3',
-            ptt: true
-        }, { quoted: mek });
+async(conn, mek, m,{from, l, prefix, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if (!q) return await reply(imgmsg)
+let wm = `© HANSAMAL-MD v${require("../package.json").version} (Test)\nWABOT MADE BY IMALKA HANSAMAL ッ`
+let buttons = [
+        {
+          "name": "quick_reply",
+          "buttonParamsJson": JSON.stringify({
+            display_text: "MENU",
+            id: `.menu`
+          })
+        },
+        {
+          "name": "quick_reply",
+          "buttonParamsJson": JSON.stringify({
+            display_text: "PING",
+            id: `.ping`
+          })
+        }
+        ];
 
-        // Then send the image with the alive message
-        await conn.sendMessage(from, {
+            await conn.sendMessage(from, {
             image: { url: config.ALIVE_IMG },
             caption: config.ALIVE_MSG
         }, { quoted: mek });
@@ -28,3 +52,4 @@ async(conn, mek, m, {from, quoted, body, isCmd, command, args, q, isGroup, sende
         reply(`Error: ${e}`);
     }
 })
+
