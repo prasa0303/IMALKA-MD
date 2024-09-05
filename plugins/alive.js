0cef172cdb9f621a2d69528210bfe65b167121df
HANSAMAL OFFICIAL <atayafuataya@gmail.com>
2024-09-05 19:51:42 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,24 +1,14 @@
 const config = require('../config')
 const { cmd, commands } = require('../command')
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
-const { Maker } = require('imagemaker.js')
-var imgmsg = "```Please give me a name !```"
-var imgmsg2 = "*Ex: .banner imalka+hansamal*"
-var imgmsg3 = "*Ex: .banner2 imalka+hansamal*"
-var imgmsg4 = "*Ex: .banner3 imalka+hansamal*"
-var imgmsg5 = "*Ex: .banner3 imalka+hansamal*"
-var imgmsg6 = "*Ex: .banner3 imalka+hansamal*"
-var desc = "It creates ephoto360 logos using 1 word.."
-var desc2 = "It creates channel banners.."
-var errt = "*I cant create this logo :(*"
 
 cmd({
-    pattern: "logo",
-    react: '🎭',
-    alias: ["logo1","ephoto360","ephoto"],
+    pattern: "alive",
+    react: '❤️',
+    alias: ["life"],
     desc: desc,
-    category: "logo",
-    use: '.ephoto360 ayodya vichakshana',
+    category: "main",
+    use: 'imalka',
     filename: __filename
 },
 async(conn, mek, m,{from, l, prefix, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
