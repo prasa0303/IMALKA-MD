71a5d75c7243790139505dba18799f096c30fa88
HANSAMAL OFFICIAL <atayafuataya@gmail.com>
2024-09-16 07:24:23 +0530

    Create logo.js

---
+++ plugins/logo.js
@@ -0,0 +1,15 @@
+const maker = require('mumaker')
+
+module.exports = {
+    pattern: "3dneon",
+    alias: ["3dn"],
+    desc: "Make text logo.",
+    react: "🍁",
+    category: "main",
+    start: async(Miku, m,{pushName,prefix,text}) => {
+        if(!text) return m.reply(`Example: *${prefix}3dneon Atlas Bot*`);
+        maker.textpro("https://textpro.me/create-3d-neon-light-text-effect-online-1028.html", [
+    `${text}`,]).then((data) => Miku.sendMessage(m.from, { image: { url: data }, caption: `Made by ${botName}` }, { quoted: m }))
+    .catch((err) => m.reply('An Error occued !'));
+    }
+}
