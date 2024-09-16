e1ba04b65f9272c97b51ffa5209f02c1964c1918
HANSAMAL OFFICIAL <atayafuataya@gmail.com>
2024-09-16 09:31:21 +0530

    Create yts.js

---
+++ plugins/yts.js
@@ -0,0 +1,104 @@
+const {cmd , commands} = require('../command')
+const fg = require('api-dylux')
+const yts = require('yt-search')
+cmd({
+    pattern: "song",
+    desc: "To download songs.",
+    react: "🎵",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("Please give me a url or title")  
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url
+    
+    
+let desc = `
+*HANSAMAL-MD SONG DOWNLOADER*
+
+🎵 *Song Found!* 
+
+➥ *Title:* ${data.title} 
+➥ *Duration:* ${data.timestamp} 
+➥ *Views:* ${data.views} 
+➥ *Uploaded On:* ${data.ago} 
+➥ *Link:* ${data.url} 
+
+🎧 *Enjoy the music brought to you by* *HANSAMAL-MD*! 
+
+> *Created with ❤️ by IMALKA HANSAMAL* 
+
+> *© HANSAMAL - MD* 
+*💻 GitHub:* https://github.com/cobrs11/HANSAMAL-MD/
+`
+
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+
+//download audio
+
+let down = await fg.yta(url)
+let downloadUrl = down.dl_url
+
+//send audio message
+await conn.sendMessage(from,{audio: {url:downloadUrl},mimetype:"audio/mpeg",fileName:data.title + ".mp3",caption:"*HANSAMAL-MD*"},{quoted:mek})
+
+}catch(e){
+console.log(e)
+  reply('${e}')
+}
+})
+
+//====================video_dl=======================
+
+cmd({
+    pattern: "video",
+    desc: "To download videos.",
+    react: "🎥",
+    category: "download",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+if(!q) return reply("Please give me a url or title")  
+const search = await yts(q)
+const data = search.videos[0];
+const url = data.url
+    
+    
+let desc = `
+*HANSAMAL-MD VIDEO DOWNLOADER*
+
+🎥 *Video Found!* 
+
+➥ *Title:* ${data.title} 
+➥ *Duration:* ${data.timestamp} 
+➥ *Views:* ${data.views} 
+➥ *Uploaded On:* ${data.ago} 
+➥ *Link:* ${data.url} 
+
+🎬 *Enjoy the video brought to you by* *HANSAMAL_MD*! 
+
+> *Created with ❤️ by IMALKA HANSAMAL* 
+
+> *© HANSAMAL- MD* 
+*💻 GitHub:* https://github.com/cobrs11/HANSAMAL-MD/
+`
+
+await conn.sendMessage(from,{image:{url: data.thumbnail},caption:desc},{quoted:mek});
+
+//download video
+
+let down = await fg.ytv(url)
+let downloadUrl = down.dl_url
+
+//send video message
+await conn.sendMessage(from,{video: {url:downloadUrl},mimetype:"video/mp4"},{quoted:mek})
+
+}catch(e){
+console.log(e)
+  reply('${e}')
+}
+})
