4310cf610e8638577f47c418e22ad6649ba42e81
HANSAMAL OFFICIAL <atayafuataya@gmail.com>
2024-09-20 13:14:07 +0530

    Update logo.js

--- plugins/logo.js
+++ plugins/logo.js
@@ -1,22 +1,27 @@
-const mumaker = require('mumaker'); 
-const { cmd } = require('../command');
-const config = require('../config'); // Ensure your API key is in config
-
+const config = require('../config')
+const { cmd, commands } = require('../command')
+const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
+const { Maker } = require('imagemaker.js')
 cmd({
     pattern: "logo",
-    desc: "Generate a logo for the WhatsApp bot.",
+    react: '🎭',
+    alias: ["logo1","ephoto360","ephoto"],
+    desc: desc,
     category: "other",
-    react: "🎨",
+    use: '.ephoto360 hansamal',
     filename: __filename
 },
-async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
-    try {
-        const botName = args.join(' ').trim() || ".logo";
-        const logoUrl = `https://en.ephoto360.com/create-dragon-ball-style-text-effects-online-809.html?text=${encodeURIComponent(botName)}`;
-
-        await conn.sendMessage(from,{IMG: {url:downloadUrl},mimetype:"logo/jpg",fileName:data.title + ".jpg",caption:"*© HANSAMAL-MD*"},{quoted:mek})
-   }catch(e){
-console.log(e)
-  reply('${e}')
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+    await conn.sendMessage(from, { react: { text: '🎆', key: mek.key }})
+    if (!q.includes('+')) return await reply(imgmsg4)
+    let [name,name2] = q.split('+')
+    new Maker().Ephoto360('https://en.ephoto360.com/create-glossy-silver-3d-text-effect-online-802.html', [`${name}`,`${name2}`]).then(async res => {
+        await conn.sendMessage(from, { image: await getBuffer(res.imageUrl), caption: HANSAMAL-MD }, { quoted: mek })
+        await conn.sendMessage(from, { react: { text: '✔', key: mek.key }})
+    })
+} catch (e) {
+    reply(errt)
+  l(e)
 }
-});
+})
