5f62d412236b8032f68d843a2b925b77813fbac1
HANSAMAL OFFICIAL <atayafuataya@gmail.com>
2024-09-20 12:36:48 +0530

    Update logo.js

--- plugins/logo.js
+++ plugins/logo.js
@@ -14,8 +14,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         const botName = args.join(' ').trim() || ".logo";
         const logoUrl = `https://en.ephoto360.com/create-dragon-ball-style-text-effects-online-809.html?text=${encodeURIComponent(botName)}`;
 
-        await reply(`🎨 Click [here](${logoUrl}) to create your logo!`);
-        await conn.sendMessage(from,{document: {url:downloadUrl},mimetype:"logo/jpg",fileName:data.title + ".jpg",caption:"*© HANSAMAL-MD*"},{quoted:mek})
+        await conn.sendMessage(from,{IMG: {url:downloadUrl},mimetype:"logo/jpg",fileName:data.title + ".jpg",caption:"*© HANSAMAL-MD*"},{quoted:mek})
    }catch(e){
 console.log(e)
   reply('${e}')
